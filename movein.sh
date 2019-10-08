df10b5d197c3d0a8642d2ef2121ec341c6288435
sk3l <mike@skelton.onl>
2019-10-08 12:02:13 -0400

    Formatting improvements.

--- movein.sh
+++ movein.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Utility functions
 function usage {
 	echo -e "\nUsage:\n"
 	echo -e "    movein.sh [-d distro] [-l logfile] [-u user] <script1> <script2> ..."
@@ -9,6 +10,18 @@ function usage {
     echo -e "   -l logfile = location of movein log"
     echo -e "   -u user    = name of base movein user"
     echo -e "   <scriptN>  = movein scripts to execute"
+}
+
+NO_COLOR="\033[0m"
+INFO_COLOR="\033[1;32m"
+ERR_COLOR="\033[0;31m"
+
+function log_info {
+    echo -e "${INFO_COLOR}$1${NO_COLOR}"
+}
+
+function log_error {
+    echo -e "${ERR_COLOR}$1${NO_COLOR}"
 }
 
 # Variables
@@ -38,47 +51,47 @@ shift "$((OPTIND-1))"
 # Write output to the log file and stdout
 exec &> >(tee -a "$LOG")
 
-echo -e "\n====== Starting movein ======"
-if [[ -f $HOME/.movedin ]]; then
-	echo -e "Already moved in; returning\n"
-	return 0
-fi
+log_info "\n====== Starting movein ======"
+#if [[ -f $HOME/.movedin ]]; then
+#	echo -e "Already moved in; returning\n"
+#	exit 0
+#fi
 
-echo -e "\n~~~ Examining host Linux distro ~~~\n"
+log_info "\n~~~ Examining host Linux distro ~~~\n"
 
 OS_TYPE=""
 OS_VERSION=""
 if [[ -f /etc/redhat-release ]]; then
     if grep 'CentOS' /etc/redhat-release; then
-        echo -e "* Detected CentOS  distro *"
+        log_info "* Detected CentOS  distro *"
 	    OS_TYPE="centos"
     else
-        echo -e "* Detected Red Hat Enterprise Linux distro *"
+        log_info "* Detected Red Hat Enterprise Linux distro *"
 	    OS_TYPE="rhel"
     fi
 elif [[ $(lsb_release) ]]; then
-	echo -e "* Detected Ubuntu distro *"
+	log_info "* Detected Ubuntu distro *"
 	OS_TYPE="ubuntu"
 else
-	echo -e "!!! Detected unknown distro!!!"
-    echo -e "Aborting movein due to unrecognized OS"
+	log_error "!!! Detected unknown distro!!!"
+    log_error "Aborting movein due to unrecognized OS"
     exit 1
 fi
 
 # Source user baseline script
-echo -e "\n~~~ Sourcing base user ~~~\n"
+log_info "\n~~~ Sourcing base user ~~~\n"
 source crates/base/user
 
 # Source per-distro baseline script
-echo -e "\n~~~ Sourcing base env for ${OS_TYPE}  ~~~\n"
+log_info "\n~~~ Sourcing base env for ${OS_TYPE}  ~~~\n"
 source crates/${OS_TYPE}/${OS_TYPE}_base
 
 # Custom scripts from argv are sourced here
-echo -e "\n~~~ Sourcing scripts from command-line  ~~~\n"
+log_info "\n~~~ Sourcing scripts from command-line  ~~~\n"
 for SCRIPT in $@; do
-    echo -e "Sourcing $SCRIPT"
+    log_info "Sourcing $SCRIPT"
     source $SCRIPT
 done
 
-echo $(date) > $HOME/.movedin
-echo -e "\n====== Movein has completed ======\n"
+#echo $(date) > $HOME/.movedin
+log_info "\n====== Movein has completed ======\n"
